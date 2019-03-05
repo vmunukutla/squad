bec6d867003a9eca4f8c7fe92780307205479498
vmunukutla <vikas9.m@gmail.com>
2019-03-05 15:00:34 -0800

    updated test.py to take in character embedding vectors

--- test.py
+++ test.py
@@ -40,10 +40,11 @@ def main(args):
     # Get embeddings
     log.info('Loading embeddings...')
     word_vectors = util.torch_from_json(args.word_emb_file)
+    char_vectors = util.torch_from_json(args.char_emb_file)
 
     # Get model
     log.info('Building model...')
-    model = BiDAF(word_vectors=word_vectors,
+    model = BiDAF(word_vectors=word_vectors, char_vectors=char_vectors,
                   hidden_size=args.hidden_size)
     model = nn.DataParallel(model, gpu_ids)
     log.info('Loading checkpoint from {}...'.format(args.load_path))
@@ -130,7 +131,7 @@ def main(args):
     # Write submission file
     sub_path = join(args.save_dir, args.split + '_' + args.sub_file)
     log.info('Writing submission file to {}...'.format(sub_path))
-    with open(sub_path, 'w') as csv_fh:
+    with open(sub_path, 'w', newline='', encoding='utf-8') as csv_fh:
         csv_writer = csv.writer(csv_fh, delimiter=',')
         csv_writer.writerow(['Id', 'Predicted'])
         for uuid in sorted(sub_dict):
