6dd564a9b55fee2b7a1cccee4968a9cf80c40198
vmunukutla <vikas9.m@gmail.com>
2019-03-16 16:07:48 -0700

    asdf

--- models.py
+++ models.py
@@ -45,7 +45,7 @@ class BiDAF(nn.Module):
 
         self.emb_encoder = layers.EmbeddingEncoder(drop_prob=drop_prob)
 
-        self.att = layers.BiDAFAttention(hidden_size=hidden_size,
+        self.att = layers.BiDAFAttention(hidden_size=2*hidden_size,
                                          drop_prob=drop_prob)
 
         self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
@@ -59,8 +59,8 @@ class BiDAF(nn.Module):
                                       drop_prob=drop_prob)
 
 
-        self.conv_layer = nn.Conv1d(hidden_size * 4, hidden_size, 7, padding=math.floor(7/2))
-        self.out = layers.QANet(hidden_size=hidden_size)
+        # self.conv_layer = nn.Conv1d(hidden_size * 4, hidden_size, 7, padding=math.floor(7/2))
+        # self.out = layers.QANet(hidden_size=hidden_size)
 
     def forward(self, cw_idxs, qw_idxs, cc_idxs, qc_idxs):
         c_mask = torch.zeros_like(cw_idxs) != cw_idxs
@@ -86,8 +86,8 @@ class BiDAF(nn.Module):
         print(q_emb)
         print(q_emb.shape)
 
-        c_enc = self.emb_encoder(c_emb, c_mask) # (batch_size, c_len, hidden_size)
-        q_enc = self.emb_encoder(q_emb, q_mask) # (batch_size, c_len, hidden_size)
+        # c_enc = self.emb_encoder(c_emb, c_mask) # (batch_size, c_len, hidden_size)
+        # q_enc = self.emb_encoder(q_emb, q_mask) # (batch_size, c_len, hidden_size)
         # print('c_enc')
         # print(c_enc)
         # print(c_enc.shape)
@@ -95,8 +95,8 @@ class BiDAF(nn.Module):
         # print(q_enc)
         # print(q_enc.shape)
 
-        # c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
-        # q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
 
         att = self.att(c_enc, q_enc,
                        c_mask, q_mask)    # (batch_size, c_len, 4 * hidden_size)
@@ -104,17 +104,17 @@ class BiDAF(nn.Module):
         # print('att')
         # print(att)
         # print(att.shape)
-        att = att.permute(0, 2, 1)
-        att = self.conv_layer(att)
+        # att = att.permute(0, 2, 1)
+        # att = self.conv_layer(att)
+        #
+        # att = att.permute(0, 2, 1)
+        #
+        # M1, M2, M3 = self.model_encoder(att, c_mask)
 
-        att = att.permute(0, 2, 1)
+        mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size)
 
-        M1, M2, M3 = self.model_encoder(att, c_mask)
+        # out = self.out(M1, M2, M3, c_mask)  # 2 tensors, each (batch_size, c_len)
 
-        # mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size)
-
-        out = self.out(M1, M2, M3, c_mask)  # 2 tensors, each (batch_size, c_len)
-
-        # out = self.out(att, mod, c_mask)
+        out = self.out(att, mod, c_mask)
 
         return out
