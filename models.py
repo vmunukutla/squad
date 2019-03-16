c920d2b74487e8a5b55a5a4c7c715385530ffc1b
vmunukutla <vikas9.m@gmail.com>
2019-03-16 14:12:56 -0700

    char v3

--- models.py
+++ models.py
@@ -30,13 +30,13 @@ class BiDAF(nn.Module):
         hidden_size (int): Number of features in the hidden state at each layer.
         drop_prob (float): Dropout probability.
     """
-    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob=0.):
+    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob=0.1):
         super(BiDAF, self).__init__()
         hidden_size = 96
         self.hidden_size = hidden_size
         self.emb = layers.Embedding(word_vectors=word_vectors,
                                     char_vectors=char_vectors,
-                                    hidden_size=96,
+                                    hidden_size=hidden_size,
                                     drop_prob=drop_prob)
 
         self.enc = layers.RNNEncoder(input_size=hidden_size,
@@ -44,10 +44,9 @@ class BiDAF(nn.Module):
                                      num_layers=1,
                                      drop_prob=drop_prob)
 
-        self.emb_encoder = layers.EmbeddingEncoder()
+        # self.emb_encoder = layers.EmbeddingEncoder()
 
-        # in my version, this is 2 * 96
-        self.att = layers.BiDAFAttention(hidden_size=2 * 96,
+        self.att = layers.BiDAFAttention(hidden_size=2 * hidden_size,
                                          drop_prob=drop_prob)
 
         self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
@@ -75,8 +74,8 @@ class BiDAF(nn.Module):
         c_emb = self.emb(cw_idxs, cc_idxs)      # (batch_size, c_len, hidden_size)
         q_emb = self.emb(qw_idxs, qc_idxs)        # (batch_size, q_len, hidden_size)
 
-        # c_emb = self.emb_encoder(c_emb, c_mask)
-        # q_emb = self.emb_encoder(q_emb, q_mask)
+        # c_enc = self.emb_encoder(c_emb, c_mask) # (batch_size, c_len, hidden_size)
+        # q_enc = self.emb_encoder(q_emb, q_mask) # (batch_size, c_len, hidden_size)
 
         c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
         q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
